df173016d22848078bdb41c5b8dd969a78f9cec9
János Székely <szekelyjani@gmail.com>
2024-03-20 12:53:50 +0100

    Initial Commit

---
+++ MyMapWithRepresentable/MyMapWithRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapWithRepresentableApp.swift
+//  MyMapWithRepresentable
+//
+//  Created by János Székely on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapWithRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
